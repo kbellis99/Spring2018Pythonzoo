bf43d3e021ebd8ec9c378afaa04f00d619e82f86
Kristen <kristenbellis99@gmail.com>
2018-04-25 14:43:47 +0000

    Create Models

--- pythonzoo/zoo/admin.py
+++ pythonzoo/zoo/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from .models import Zoo, Exhibit, ExhibitNeighbors, Animal
+
 
 # Register your models here.
+
+class ZooAdmin(admin.ModelAdmin):
+	list_display = ('id', 'name')
+
+
+admin.site.register(Zoo, ZooAdmin)
+
+class ExhibitAdmin(admin.ModelAdmin):
+	list_display = ('id', 'name')
+
+
+admin.site.register(Exhibit, ExhibitAdmin)
+
+class ExhibitNeighborsAdmin(admin.ModelAdmin):
+	list_display = ('fromExhibit', 'direction', 'toExhibit')
+
+admin.site.register(ExhibitNeighbors, ExhibitNeighborsAdmin)
+
+class AnimalAdmin(admin.ModelAdmin):
+	list_display = ('id', 'name')
+
+
+admin.site.register(Animal, AnimalAdmin)
--- pythonzoo/zoo/models.py
+++ pythonzoo/zoo/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+
+class Zoo(models.Model):
+	name = models.CharField(max_length=200, help_text="Enter Zoo Name")
+	
+	def __str__(self):
+		return self.name
+	
+class Exhibit(models.Model):
+	name = models.CharField(max_length=200, help_text="Enter Exhibit Name")	
+	zoo = models.ForeignKey('Zoo', on_delete=models.SET_NULL, null=True)
+	
+	def __str__(self):
+		return self.name
+
+class ExhibitNeighbors(models.Model):
+	fromExhibit = models.ForeignKey('Exhibit', on_delete=models.SET_NULL, null=True)
+	direction = models.CharField(max_length=200)
+	toExhibit = models.ForeignKey('Exhibit', on_delete=models.SET_NULL, null=True, related_name='neighbor_of')
+	
+	def __str__(self):
+		return str(self.fromExhibit) + ': ' + str(self.direction) + ' to: ' + str(self.toExhibit)
+
+class Animal(models.Model):
+	name = models.CharField(max_length=200, help_text="Enter Animal Name")
+	zoo = models.ForeignKey('Zoo', on_delete=models.SET_NULL, null=True)
+	exhibit = models.ForeignKey('Exhibit', on_delete=models.SET_NULL, null=True)
